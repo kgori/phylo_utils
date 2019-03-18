23a58754a6a95abc97844555c556b70836bc3327
kgori <kevin.c.gori@gmail.com>
2019-03-18 15:42:33 +0000

    Reorder DNA characters

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest import TestCase
+import numpy as np
+from phylo_utils.utils import seq_to_partials
+
+A_POS=0
+C_POS=1
+G_POS=2
+T_POS=3
+
+
+class TestSeqToPartials(TestCase):
+
