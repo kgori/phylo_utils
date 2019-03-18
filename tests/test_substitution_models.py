02869cf9b333405d7f48b5b4df6952ff2730f058
kgori <kevin.c.gori@gmail.com>
2019-03-18 15:44:49 +0000

    Use import * syntax in test

--- tests/test_substitution_models.py
+++ tests/test_substitution_models.py
@@ -1,19 +1,8 @@
 import unittest
-from abc import abstractmethod
 from unittest import TestCase
 import numpy as np
-import phylo_utils as phy
 import phylo_utils.substitution_models.abstract
-from phylo_utils.substitution_models import TN93
-from phylo_utils.substitution_models.unrest import Unrest
-from phylo_utils.substitution_models.gtr import GTR
-from phylo_utils.substitution_models.hky85 import HKY85
-from phylo_utils.substitution_models.f84 import F84
-from phylo_utils.substitution_models.f81 import F81
-from phylo_utils.substitution_models.k80 import K80
-from phylo_utils.substitution_models.jc69 import JC69
-from phylo_utils.substitution_models.wag import WAG
-from phylo_utils.substitution_models.lg import LG
+from phylo_utils.substitution_models import *
 
 
 class TestInputs(TestCase):
