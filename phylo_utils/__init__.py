e1a036a733db6bc1ea779d39ac2051a09e5a503c
kgori <kevin.c.gori@gmail.com>
2019-03-18 15:43:24 +0000

    Reorder imports

--- phylo_utils/__init__.py
+++ phylo_utils/__init__.py
@@ -1,9 +1,9 @@
+from . import simulation
+from . import optimisation
+from . import discrete_gamma
 from phylo_utils.likelihood import numba_likelihood_engine
 from . import substitution_models
 from . import rate_models
 from . import tree_model
 from .utils import seq_to_partials
-from . import simulation
-from . import optimisation
-from . import discrete_gamma
 
